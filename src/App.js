56706d2dba41edb0f2175f36d01a09bca8c8e226
amy-ym-yu <amy.ym.yu3@gmail.com>
2022-08-09 23:20:40 -0700

    built list

--- src/App.js
+++ src/App.js
@@ -6,13 +6,107 @@ import { Button, Card, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  // declaring 2 variables, setting the states for text and completion
+
+    return (
+      <div>
+        <TodoList listName="main"/>
+        <TodoList listName="one" week={2} />
+        <TodoList listName="two"/>
+      </div>
+    )
+
+}
+
+function TodoList(props) {
+  // todos - js list that will hold all of the tasks
+  // setTodos is a function which
+  // useState contains current state and setter
+  console.log("props are: ", props);
   const [todos, setTodos] = React.useState([
     {
       text: "This is a sample todo",
       isDone: false
     }
-  ])
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }]; // appending text to list of tasks
+    setTodos(newTodos); // setting new state for todo variable
+  };
+
+  const markTodo = (index) => {
+    const newTodos = [...todos]; // copies entire list of tasks
+    newTodos[index].isDone = true; // marks task at index as true
+    setTodos(newTodos); // sets new state for todo variable
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos]; // copies entire list of task
+    newTodos.splice(index, 1); // returns a new array for newTodos without task at index
+    setTodos(newTodos); // sets new state for todo variable
+  };
+
+  return (
+    <div className='app'>
+      <div className='container'>
+        <h1 className='text-center mb-4'>{props.listName}</h1>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                key={index}
+                index={index}
+                todo={todo}
+                markTodo={markTodo}
+                removeTodo={removeTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+
+  function Todo({todo, index, markTodo, removeTodo}) {
+    return (
+      <div className="todo">
+        <span style={{ textDecoration: todo.isDone ? "line-through" : ""}}>{todo.text}</span>
+
+        <div>
+          <Button variant="outline-success" onClick={() => markTodo(index)}>✓</Button>{' '}
+          <Button variant="outline-danger" onClick={() => removeTodo(index)}>✕</Button>
+        </div>
+      </div>
+    );
+  }
+
+  function FormTodo({ addTodo }) {
+    const [value, setValue] = React.useState("");
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (!value) return;
+      addTodo(value);
+      setValue("");
+    };
+
+    return (
+      <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label><b>Add Todo</b></Form.Label>
+        <Form.Control type="text" className="input" value={value}
+        onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
+      </Form.Group>
+      <Button variant="primary mb-3" type="submit">Submit</Button>
+      </Form>
+    );
+  }
+
 }
+
+
 
 export default App;
