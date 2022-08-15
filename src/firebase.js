061be36b88779219957bf2ce7966fe332add0d35
amy-ym-yu <amy.ym.yu3@gmail.com>
2022-08-15 14:49:49 -0700

    adding document to baseList

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   };
   
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig); // eslint-disable-next-line
+  const app = initializeApp(firebaseConfig);
   const db = getFirestore(app); 
 
   export { db }
